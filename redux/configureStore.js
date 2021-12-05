f20afceb9ef170a0bc91b9d7d8105ed9a0db35a2
Kutay Yaman <yamankutay1@gmail.com>
2021-12-05 18:27:10 +0300

    asyncstorage is done

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,8 +1,41 @@
-import {createStore} from 'redux';
-import authReducer from './authReducer';
+import { createStore } from "redux";
+import authReducer from "./authReducer";
+import { AsyncStorage } from "react-native";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "./authActions";
+import { useState } from "react";
 
-  const configureStore = () => {
-      return createStore(authReducer);
-  } 
+const configureStore = () => {
+  const [stateInLocalStorage, setStateInLocalStorage] = useState({
+    isLoggedIn: false,
+    mail: undefined,
+    password: undefined,
+    image: undefined,
+  });
 
-  export default configureStore;
+  AsyncStorage.getItem("auth").then((authData) => {
+    if (authData) {
+      try {
+        const parsedAuthData = JSON.parse(authData);
+        if (stateInLocalStorage.isLoggedIn != parsedAuthData.isLoggedIn) {
+          //state should execute one time because if i dont use this if statement it will be execute infinitly
+          setStateInLocalStorage(parsedAuthData);
+        }
+      } catch (error) {}
+    }
+  });
+
+  const store = createStore(authReducer, stateInLocalStorage);
+
+  store.subscribe(async () => {
+    try {
+      await AsyncStorage.setItem("auth", JSON.stringify(store.getState()));
+    } catch (error) {
+      // Error saving data
+    }
+  });
+
+  return store;
+};
+
+export default configureStore;
